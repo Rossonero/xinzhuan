7317911112111791a0893bc20ce6a70140802dc9
perchouli <jp.chenyang@gmail.com>
2013-05-16 15:19:03 +0800

    ADD: admin unit

--- media/admin.py
+++ media/admin.py
@@ -4,4 +4,10 @@ from .models import *
 class MediumAdmin(admin.ModelAdmin):
     list_display = ('name', 'address', 'english_name', 'sponsor', 'introduction')
     search_fields = ['name']
+
+class UnitAdmin(admin.ModelAdmin):
+    list_display = ('name', 'region')
+    search_fields = ['name']
+
 admin.site.register(Medium, MediumAdmin)
+admin.site.register(Unit, UnitAdmin)
